2ef527b246c61ffd7c45ac3d5a83f65fba3f551b
Katarina Kivimaa <katarina.kivimaa@gmail.com>
2024-09-05 10:04:48 +0300

    Lihtsad tüübid

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+$a = 1;
+$a++;
+$a--;
+$a+=2;
+$a-=2;
+$a*=2;
+$a/=2;
+$a%=2;
+
+
+$a = 'hello ' . 'world';
+$a .='asdsds';
+$array = array(1,2,3);
+$array = [1,2,3];
+
+var_dump($array[1]);
+
+$array = [
+    'essa'=> 1,
+    'tessa' => 2,
+    'kossa' => 3,
+    5 => 4,
+    'dfwefde',
+    10 => 1.2,
+    true
+];
+
+var_dump($array['kossa']);
+
+var_dump($array);
+foreach($array as $key => $value) {
+    //echo $key . '=>' . $value . "\n";
+    echo "$key=>$value\n";
+}
+
+$name = 'Kata';
+
+$string = "hello $name\n";
+var_dump($string);
+
+
+
+?>
