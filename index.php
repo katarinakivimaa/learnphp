4b7c1150cc105d0a590d0223e0956d27510fecf7
Katarina Kivimaa <katarina.kivimaa@gmail.com>
2024-09-06 14:49:34 +0300

    static

--- index.php
+++ index.php
@@ -1,49 +1,34 @@
 <?php
 
 class Box {
-    // public function __construct($name){
-    //     echo 'Box was created and its name was ' . $name;
-    // }    
-    // these are called Magic Methods, they override things and do weird stuff
+    public static $count = 0;
+    public $id;
     public function __construct(){
-        echo "Box was created.\n";
-    }    
-
-   public function __call($name, $args){
-        var_dump($name, $args);
-    } 
-
-    public function __get($name){
-        return 'cool value';
-    } 
-
-    public function __set($name, $value){
-        var_dump($name, $value);
-    } 
-
-    public function __destruct(){
-        echo "Box was destroyed!\n";
+        $this->id=self::$count++;
     }
-
-    public function __toString(...$args){
-        // return 'cool box';
-        var_dump($args);
+    public static function setCount(){
+        var_dump(self::$count);
+        // var_dump($this->id);
+    }
+    public static function name(){
+        var_dump(static::class);
     }
 }
-// array_push 
-// function hello(){
-//     $box=new Box();
-// }
-// $box = new Box('Katarina');
-$box = new Box();
-$box->name='Katarina';
-$box->something(1,2,'asghdyud');
-$box->else(1,2,'asghdyud', true);
-var_dump($box);
-// var_dump($box->cats);
-var_dump($box->alahsdha);
-echo $box;
-$box(1,'asd','afdsdf');
-// $box = 1;
-// hello();
-// echo "final echo\n";
+
+
+class Metalbox extends Box {
+
+}
+
+MetalBox::name();
+// Box::setCount();
+// var_dump(Box::class);
+// Box::$count = 1;
+// Box::$count = 2;
+// var_dump(Box::$count, Box::$count);
+// var_dump(new Box());
+// var_dump(new Box());
+// var_dump(new Box());
+// var_dump(new Box());
+
+
