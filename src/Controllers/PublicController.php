27fa32f70ac9c00d5db6a23ffe8d09cf03284fbc
Katarina Kivimaa <katarina.kivimaa@gmail.com>
2024-09-19 09:56:34 +0300

    kaspar läks suitsule

--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -1,39 +1,58 @@
 <?php
+
 namespace App\Controllers;
 
-class PublicController {
+use PDO;
+use PDOException;
 
-    public function index() {
-        $posts = [
-            ['title'=> 'Some titties 1', 'body' => 'Some boy 1'],
-            ['title'=> 'Some titties 2', 'body' => 'Some boy 2'],
-            ['title'=> 'Some titties 3', 'body' => 'Some boy 3'],
-            ['title'=> 'Some titties 4', 'body' => 'Some boy 4'],
-        ];
-        include 'views/index.php';
+class PublicController
+{
+
+    public function index()
+    {
+        try {
+            $conn = new PDO('sqlite:db.sqlite');
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+
+        // $posts = [
+        //     ['title'=> 'Some titties 1', 'body' => 'Some boy 1'],
+        //     ['title'=> 'Some titties 2', 'body' => 'Some boy 2'],
+        //     ['title'=> 'Some titties 3', 'body' => 'Some boy 3'],
+        //     ['title'=> 'Some titties 4', 'body' => 'Some boy 4'],
+        // ];
+        // include 'views/index.php';
     }
 
-    public function us(){
+    public function us()
+    {
         $posts = [
-            ['title'=> 'Some our titties 1', 'body' => 'Some boy 1'],
-            ['title'=> 'Some our titties 2', 'body' => 'Some boy 2'],
-            ['title'=> 'Some our titties 3', 'body' => 'Some boy 3'],
-            ['title'=> 'Some our titties 4', 'body' => 'Some boy 4'],
+            ['title' => 'Some our titties 1', 'body' => 'Some boy 1'],
+            ['title' => 'Some our titties 2', 'body' => 'Some boy 2'],
+            ['title' => 'Some our titties 3', 'body' => 'Some boy 3'],
+            ['title' => 'Some our titties 4', 'body' => 'Some boy 4'],
         ];
         include 'views/us.php';
     }
 
-    public function tech() {
+    public function tech()
+    {
         $posts = [
-            ['title'=> 'Some their titties 1', 'body' => 'Some boy 1'],
-            ['title'=> 'Some their titties 2', 'body' => 'Some boy 2'],
-            ['title'=> 'Some their titties 3', 'body' => 'Some boy 3'],
-            ['title'=> 'Some their titties 4', 'body' => 'Some boy 4'],
+            ['title' => 'Some their titties 1', 'body' => 'Some boy 1'],
+            ['title' => 'Some their titties 2', 'body' => 'Some boy 2'],
+            ['title' => 'Some their titties 3', 'body' => 'Some boy 3'],
+            ['title' => 'Some their titties 4', 'body' => 'Some boy 4'],
         ];
         include 'views/tech.php';
     }
 
-    public function form() {
+    public function form()
+    {
         // dump($_GET, $_POST); //query
 
         // if(isset($_GET['flname'])){
@@ -44,12 +63,13 @@ class PublicController {
 
         // $flname = isset($_GET['flname']) ? $_GET['flname'] : null;
 
-        $flname = $_GET['flname'] ?? null; 
+        $flname = $_GET['flname'] ?? null;
         // ?? on lühike väga spetsiifiline if lause
         include 'views/form.php';
     }
 
-    public function answer(){
+    public function answer()
+    {
         echo $_POST['flname'];
     }
 }
